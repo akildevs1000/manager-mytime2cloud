eb95a9145fd0ae24937272354d850ebacaf2662c
akildevs1000 <akildevs1000@gmail.com>
2025-09-23 11:32:54 +0400

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,85 +1,228 @@
 "use client";
 
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import { Button } from "@/components/ui/button";
 
-export default function Page() {
+import { useRouter, useSearchParams } from "next/navigation";
+
+
+
+import { getBranches, getAttendnaceCount, getLogs } from "@/lib/api";
+import { Suspense, useEffect, useState } from "react";
+import StatsGrid from "@/components/StatsGrid";
+import { useCompany } from "@/context/CompanyContext";
+
+export default function PageWrapper() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Page />
+    </Suspense>
+  );
+}
+
+function Page() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const { companyId, setCompanyId } = useCompany();
+
+  const [branches, setBranches] = useState([]);
+  const [stats, setStats] = useState({
+    employeeCount: 0,
+    presentCount: 0,
+    absentCount: 0,
+    leaveCount: 0,
+    vaccationCount: 0,
+  });
+  const [selectedBranch, setSelectedBranch] = useState(null);
+  const [open, setOpen] = useState(false);
+  const [logs, setLogs] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    const id = searchParams.get("company_id");
+    if (id && id !== companyId) {
+      setCompanyId(id);
+    }
+  }, [searchParams, companyId, setCompanyId]);
+
+  // Fetch branches
+  useEffect(() => {
+    if (!companyId) return;
+    const fetchBranches = async () => {
+      try {
+        const data = await getBranches(companyId);
+        setBranches(data);
+      } catch (error) {
+        console.error("Error fetching branches:", error);
+      }
+    };
+    fetchBranches();
+  }, [companyId]);
+
+  // Fetch stats
+  useEffect(() => {
+    if (!companyId) return;
+    const fetchStats = async () => {
+      try {
+        const data = await getAttendnaceCount(companyId, selectedBranch);
+        setStats(data);
+      } catch (error) {
+        console.error("Error fetching stats:", error);
+      }
+    };
+    fetchStats();
+  }, [companyId, selectedBranch]);
+
+  const handleSelectBranch = (currentValue) => {
+    if (currentValue === "Select All") {
+      setSelectedBranch(null);
+    } else {
+      const selectedBranchItem = branches.find((b) => b.name === currentValue);
+      if (selectedBranchItem) {
+        setSelectedBranch(
+          selectedBranchItem.id === selectedBranch ? null : selectedBranchItem.id
+        );
+      }
+    }
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    if (!companyId) return;
+
+    const fetchLogs = async () => {
+      try {
+        const { data } = await getLogs(companyId, page); // use page here
+        setLogs(data);
+      } catch (error) {
+        console.error("Error fetching logs:", error);
+      }
+    };
+
+    fetchLogs();
+  }, [companyId, page]); // fetch again when page changes
+
+  // Always call hooks first, then conditionally render
+  if (!companyId) {
+    return <div className="text-center">Loading....</div>;
+  }
+
   return (
     <>
-      {/* Branch Selector */}
       <div className="mb-5">
-        <Select >
-          <SelectTrigger className="w-full py-5">
-            <SelectValue placeholder="Select Branch" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="all">All Branches</SelectItem>
-            <SelectItem value="a">Branch A</SelectItem>
-            <SelectItem value="b">Branch B</SelectItem>
-          </SelectContent>
-        </Select>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              role="combobox"
+              aria-expanded={open}
+              className="w-full justify-between py-5 text-gray-500 border-none"
+            >
+              {selectedBranch
+                ? branches.find((b) => b.id === selectedBranch)?.name
+                : "Select Branch"}
+
+              {/* Arrow icon */}
+              <span className="material-icons text-gray-400 ml-2">
+                expand_more
+              </span>
+            </Button>
+          </PopoverTrigger>
+
+          <PopoverContent className="w-[320px] p-0">
+            <Command>
+              <CommandInput placeholder="Search branch..." />
+              <CommandEmpty>No branch found.</CommandEmpty>
+              <CommandGroup>
+                <CommandItem
+                  className="text-gray-500"
+                  value="Select All"
+                  onSelect={handleSelectBranch}
+                >
+                  Select All
+                </CommandItem>
+                {branches.map((branch) => (
+                  <CommandItem
+                    className="text-gray-500"
+                    key={branch.id}
+                    value={branch.name}
+                    onSelect={handleSelectBranch}
+                  >
+                    {branch.name}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </Command>
+          </PopoverContent>
+        </Popover>
       </div>
 
-      {/* Stats Grid */}
-      <div className="grid grid-cols-2 gap-4">
-        {/* Today Presents */}
-        <div className="bg-white p-2 rounded-lg shadow-md flex flex-col items-center justify-center">
-          <div className="w-12 h-12 rounded-full bg-green-100 flex items-center justify-center mb-4">
-            <span className="material-icons text-green-500">person</span>
-          </div>
-          <p className="text-1xl font-bold text-green-500">250</p>
-          <p className="text-sm text-gray-500 mt-1">Today Presents</p>
-        </div>
+      <StatsGrid stats={stats} />
 
-        {/* Today Absents */}
-        <div className="bg-white p-2 rounded-lg shadow-md flex flex-col items-center justify-center">
-          <div className="w-12 h-12 rounded-full bg-red-100 flex items-center justify-center mb-4">
-            <span className="material-icons text-red-500">person_off</span>
-          </div>
-          <p className="text-1xl font-bold text-red-500">251</p>
-          <p className="text-sm text-gray-500 mt-1">Today Absents</p>
-        </div>
 
-        {/* On Vacation */}
-        <div className="bg-white p-2 rounded-lg shadow-md flex flex-col items-center justify-center">
-          <div className="w-12 h-12 rounded-full bg-blue-100 flex items-center justify-center mb-4">
-            <span className="material-icons text-blue-500">flight</span>
-          </div>
-          <p className="text-1xl font-bold text-blue-500">0</p>
-          <p className="text-sm text-gray-500 mt-1">On Vacation</p>
+      <section>
+        <h2 className="text-lg font-bold my-5">Live Logs</h2>
+        <div className="space-y-3">
+          {logs.map((log, index) => (
+            <div key={index}
+              className="flex items-center gap-4 p-3 bg-white dark:bg-card-dark rounded-lg"
+            >
+              <img
+                alt="Liam Johnson"
+                className="h-12 w-12 rounded-full object-cover"
+                src={
+                  log?.employee?.profile_picture ||
+                  // 'https://lh3.googleusercontent.com/aida-public/AB6AXuBaC2gcwDY0mzgyIjjqALcls2KjjWNR8LPN5xvbr4zzNPRzaG9pDZy_iEdeYFYOeOISgsze_BRmzu1RtXTuL3efEqXU7nAfZPSU2LaAK9Ff9qPC42kTstmhp7QRfSBmauqlAUwSJKOfbHcF8QPXPHTM08pakvME7E-91XNPqxznhb60y7_58DhTJEooIqqs_mhq94B3T-PbbL_bQT5vQ9mYnziHXqf4WvG4stIWyrVO3G47Sed90Vs2vZa8BHjqIor97MS_3nhDzds'
+                  `https://ui-avatars.com/api/?name=${encodeURIComponent(
+                    log?.employee?.first_name == "---" ? "??" : log?.employee?.first_name
+                  )}&background=4F46E5&color=fff&size=120`
+                }
+              />
+              <div className="flex-1">
+                <p className="font-semibold">{log?.employee?.first_name}</p>
+                <p className="text-sm text-gray-500">
+                  Checked {log.log_type == null ? "---" : log.log_type}
+                </p>
+              </div>
+              <div className="text-right">
+                <p className="font-medium text-sm">{log.time}</p>
+                <p className="text-xs text-gray-500">
+                  {log.date}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
+      </section>
 
-        {/* On Leave */}
-        <div className="bg-white p-2 rounded-lg shadow-md flex flex-col items-center justify-center">
-          <div className="w-12 h-12 rounded-full bg-yellow-100 flex items-center justify-center mb-4">
-            <span className="material-icons text-yellow-500">timer</span>
-          </div>
-          <p className="text-1xl font-bold text-yellow-500">0</p>
-          <p className="text-sm text-gray-500 mt-1">On Leave</p>
-        </div>
+      <div className="flex justify-between mb-20 mt-5">
+        <button
+          onClick={() => setPage(prev => Math.max(prev - 1, 1))}
+          disabled={page === 1}
+          className="px-2 py-1 bg-gray-200 rounded disabled:opacity-50"
+        >
+          Previous
+        </button>
 
-        {/* Offline Devices */}
-        <div className="bg-white p-2 rounded-lg shadow-md flex flex-col items-center justify-center">
-          <div className="w-12 h-12 rounded-full bg-orange-100 flex items-center justify-center mb-4">
-            <span className="material-icons text-orange-500">devices_other</span>
-          </div>
-          <p className="text-1xl font-bold text-orange-500">7</p>
-          <p className="text-sm text-gray-500 mt-1">Offline Devices</p>
-        </div>
+        <span className="px-2 py-1">Page {page}</span>
 
-        {/* Leave Requests */}
-        <div className="bg-white p-2 rounded-lg shadow-md flex flex-col items-center justify-center">
-          <div className="w-12 h-12 rounded-full bg-gray-200 flex items-center justify-center mb-4">
-            <span className="material-icons text-gray-500">badge</span>
-          </div>
-          <p className="text-1xl font-bold text-gray-500">0</p>
-          <p className="text-sm text-gray-500 mt-1">Leave Requests</p>
-        </div>
+        <button
+          onClick={() => setPage(prev => prev + 1)}
+          className="px-2 py-1 bg-indigo-600 text-white rounded"
+        >
+          Next
+        </button>
       </div>
     </>
   );
